5c17c4f0d499ac33ff4fa4b14f8e7b4bbe42156c
michael peters <petersshedrach@gmail.com>
2021-09-14 20:49:24 -0300

    resolução-repositorio

---
+++ quinzena1/lista-logica/exercicios.js
@@ -0,0 +1,90 @@
+// Exemplos
+
+// Exercício 0A
+function soma() {
+    // escreva seu código aqui
+    const num1 = prompt('Digite o primeiro número')
+    const num2 = prompt('Digite o segundo número')
+
+    console.log(Number(num1) + Number(num2))
+}
+
+// Exercício 0B
+function imprimeMensagem() {
+    // escreva seu código aqui
+    const mensagem = prompt('Digite sua mensagem')
+
+    console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+// Exercício 1
+function calculaAreaRetangulo() {
+    // escreva seu código aqui
+    const alt = Number(prompt("qual altura do retangulo?"))
+    const larg = Number(prompt("qual largura do retangulo?"))
+    let area = Number(alt) * Number(larg)
+    console.log(Number(area))
+}
+
+// Exercício 2
+function imprimeIdade() {
+    // escreva seu código aqui
+    const anoNasc = Number(promp("qual ano você nasceu?"))
+    const anoAtual = Number(promp("qual ano estamos?"))
+    const idade = Number(anoNasc) - Number(ano)
+
+
+}
+// Exercício 3
+function calculaIMC() {
+    // escreva seu código aqui
+}
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+    // escreva seu código aqui
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+    // escreva seu código aqui
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+    // escreva seu código aqui
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+    // escreva seu código aqui
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+    // escreva seu código aqui
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+    // escreva seu código aqui
+}
+
+// Exercício 10
+function checaRenovacaoRG() {
+    // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+    // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+    // escreva seu código aqui
+}
---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,55 @@
+//____________________ - /*AULA DO BRUNO MÓVEL EXERCICIO 4*/
+
+
+//________________________EXERCICIO_____________________________________________________
+/*_____________________Exercícios de interpretação de código___________________
+
+__________________________RESPOSTAS:
+1- a.false_   b.false_   c.true_    d.boolean
+*/
+//________________________________________________________________
+//________________________RESPOSTAS:
+/*2-
+const num1 = (prompt("insira o primeiro numero:"))
+const num2 = (prompt("insira o segundo numero:"))
+console.log("soma:", Number(num1) + Number(num2))
+_______*/
+//____________________Exercícios de escrita de código_______________________
+//________________________RESPOSTAS:-________________________________________
+/*1-
+  a)const suaIdade = (prompt("insira sua idade:"))
+  b)const idadeAmigo = (prompt("insira a idade do seu melhor amigo:"))
+  c)console.log("Sua idade é maior do que a do seu melhor amigo?", Number(suaIdade) > Number(idadeAmigo))
+  d)console.log("qual a diferença de idade entre você e seu melhor amigo?", Number(suaIdade) - Number(idadeAmigo))
+*/ //________________________RESPOSTAS:________________________________________
+/*2-
+   a)const numPar = (prompt("insira um numero par"))
+   console.log(numPar)
+   console.log("resultado da divisão por 2 é:", Number(numPar) / Number(2))
+   let restoDaDivisao = Number(numPar) % Number(2)
+
+   b)console.log("o restante do que sobrou da divisão por 2 é:", restoDaDivisao)
+   c) em calculos com N°pares identifiquei que o calculo é perfeito restando apenas e sempre "0"
+   d) caso seja posto o N°impar o calculo é aproveitado até o limite da margem pós virgulas
+*/
+//________________________________________________________________
+//_________________________RESPOSTAS:_______________________________________________
+/*3-________const idadeUsuario = (prompt("insira sua idade"))
+____________const meses = 12
+____________const dias = 365
+____________const horas = 24
+a)console.log("a sua idade em meses é:", Number(idadeUsuario) * Number(meses))
+b)console.log("a sua idade em dias é:", Number(idadeUsuario) * Number(dias))
+c)console.log("a sua idade em horas é:", Number(dias) / Number(meses) * Number(horas) * Number(meses)) */
+//__________________________RESPOSTAS:_______________________________________________
+/*4-___
+        const num1 = (prompt("insira o primeiro numero:"))
+       const num2 = (prompt("insira o segundo numero:"))
+
+       let resta1 = Number(num1) % Number(num2)
+        let resta2 = Number(num2) % Number(num1)
+
+console.log("o primeiro numero é maior que o segundo numero?", Number(num1) > Number(num2))
+console.log("o primeiro numero é igual ao segundo numero?", Number(num1) === Number(num2))
+console.log("o primeiro numero é divisível pelo segundo numero?", Number(resta1) === Number(0))
+console.log("o segundo numero é divisível pelo primeiro numero?", Number(resta2) === Number(0))*/
