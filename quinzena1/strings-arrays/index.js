e392eb89576cec9e790f15ccdec99455b67987e8
michael peters <petersshedrach@gmail.com>
2021-09-14 23:52:55 -0300

    quinzena1-aula05

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,65 @@
+//_______________________________exercicio:
+/*Exercícios de interpretação de código        
+               RESPOSTAS
+1-
+span className="badge">8</span>a.  undefined
+index.js:5 b.  null
+index.js:8 c.  11
+index.js:11 d.  3
+index.js:14 e.  (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]0: 31: 192: 53: 64: 75: 86: 97: 108: 119: 1210: 13length: 11[[Prototype]]: Array(0)
+index.js:17 f.  9*/
+//             ---------------------
+
+/**1-
+ *SUBI NUM ÔNIBUS EM MIRROCOS 27
+    
+
+               RESPOSTAS
+/*Exercícios de escrita de código
+*/
+//1-
+
+/*const nome = prompt("qual o seu nome?")
+const email = prompt("qual o seu e-mail?")
+console.log("O e-mail", email, " foi cadastrado com sucesso. Seja bem vinda(o)", nome, "!")
+*/
+//2-
+//A)
+/*const comidasFavoritas = ['arroz doce', 'yakisoba', 'camarão', 'lula', 'sushi']
+console.log(comidasFavoritas)*/
+//B)
+/*const comidasFavoritas2 = ['arroz doce',
+    'yakisoba',
+    'camarão',
+    'lula',
+    'sushi'
+]
+console.log("Essas são as minhas comidas preferidas:", comidasFavoritas2)*/
+
+
+//C)
+/*const comidaFavoritaDoUsuario = prompt("qual sua comida favorita?")
+    //const identificaçaodacomida = (comidasFavoritas2.splice(2, ))
+console.log(comidasFavoritas2.replaceAll("camarão", comidaFavoritaDoUsuario))          ??????????????????????????????????????????????????????????????????????????
+    //const adicionacomida = (comidaFavoritaDoUsuario.)
+console.log(troca)*/
+
+
+//a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`
+const listaDeTarefas = [1, 2, 3]
+
+
+//b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+const tarefasUsuario1 = prompt("digite a 1° tarefa do seu dia.")
+const tarefasUsuario2 = prompt("digite a 2° tarefa do seu dia.")
+const tarefasUsuario3 = prompt("digite a 3° tarefa do seu dia.")
+const lista1 =
+    const lista2 =
+        const lista3 =
+            //c) Imprima o array na tela
+            console.log(listaDeTarefas)
+            //d) Peça ao usuário que digite o **índice** de uma tarefa que ele já realizou: 0, 1 ou 2 
+
+//e) Remova da lista o item de índice que o usuário escolheu.
+
+//f) Imprima o array na tela
