140a121d11a2e0357172d0c2bab29dfd648306f1
michael peters <petersshedrach@gmail.com>
2021-09-16 21:40:12 -0300

    quinzena2-aula06

---
+++ quinzena2/funcoes/index.js
@@ -0,0 +1,38 @@
+// --- EXERCÍCIO 1 ------------------------------------------------------------------
+
+/*function minhaFuncao(variavel) { 
+    return variavel * 5 }
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))*/
+
+/*A) Os valores referentes a multiplicação da function minhaFunção X valor atribuido para cada variável,
+respectivamente 5*2=10 e 5*10=50.*/
+/* B) Declarar a função não executa o código. Invalid or unexpected token */
+
+//--- EXERCÍCIO 2 ------------------------------------------------------------------
+
+/*let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+
+
+/*A) Essa função lê o texto de forma uniforme em letra minuscula, e, pesquisa se há a palavra "cenoura", retornando como true ou false. 
+/*B)
+i. true- cenoura em letra minuscula e consta na frase escrita.cenoura.
+i.true- CENOURA convertido para letra minuscula, não interfere na busca e consta na frase.
+iii. true- Cenouras no plural não interfere no resultado da busca.*/
+
+
+// --- EXERCÍCIO 3 ------------------------------------------------------------------
+
+
+
+// --- EXERCÍCIO 4 ------------------------------------------------------------------
+
+
+// --- EXERCÍCIO 5 ------------------------------------------------------------------
