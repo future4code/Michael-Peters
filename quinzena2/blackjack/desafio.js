075815e900249363f6e512f8ee153709fcebf2f7
michael peters <petersshedrach@gmail.com>
2021-10-05 20:43:07 -0300

    quinzena3-revisao-jsrevisao-js

---
+++ quinzena2/blackjack/desafio.js
@@ -0,0 +1,119 @@
+// /**
+//  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+//  * 
+//  * 
+//     const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+
+//     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+//     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+//  * 
+//  * 
+//  * 
+//  */
+
+
+
+
+
+
+
+
+// const confirme = prompt("Quer iniciar uma nova rodada ? digite sim ou não para puxar uma carta.")
+// const mensagemDeBoasVindas = "Bem vindo ao jogo de Blackjack!"
+// const mensagemAcabou = "O jogo acabou"
+// const cancel = confirme === "não"
+// const ok = confirme === "sim"
+
+
+// console.log(mensagemDeBoasVindas)
+
+// if (cancel) {
+//     console.log(mensagemAcabou)
+// }
+
+// function usuariocarta1() {
+//     const confirmaçãoSim = confirme === "sim"
+//     if (ok) {
+//         if (confirmaçãoSim) {
+//             const carta = comprarCarta()
+//             console.log("carta 1 do usuario é:", carta.texto, carta.valor)
+//             console.log()
+//             return valor = Number(carta.valor)
+//         }
+//     }
+
+// }
+
+
+// function usuariocarta2() {
+
+
+//     const confirmaçãoSim = confirme === "sim"
+//     if (ok) {
+//         if (confirmaçãoSim) {
+//             const carta = comprarCarta()
+//             console.log("carta 2 do usuario é:", carta.texto, carta.valor)
+//             console.log()
+//             return valor = Number(carta.valor)
+
+//         }
+//     }
+
+
+// }
+
+
+
+// function carta1computador() {
+
+
+//     if (ok) {
+//         const carta = comprarCarta()
+//         console.log("carta 1 do computador é:", carta.texto, carta.valor)
+
+//         return valor = Number(carta.valor)
+//     }
+// }
+
+
+
+
+// function carta2computador() {
+
+//     if (ok) {
+//         const carta = comprarCarta()
+//         console.log("carta 2 do computador é:", carta.texto, carta.valor)
+
+//         return valor = Number(carta.valor)
+
+//     }
+// }
+
+
+
+
+// Number(usuariocarta1())
+// Number(usuariocarta2())
+// Number(carta1computador())
+// Number(carta2computador())
+
+
+
+// console.log("resul", resultadoUser)
+
+
+
+
+// const resultadoUser = Number(usuariocarta1()) + Number(usuariocarta2())
+// console.log("resul", resultadoUser)
+//     // let const resultadoComputer = carta1computador() + carta2computador()
+
+// // const maiorPontuação = resultadoUser > resultadoComputer
+// // const igualPontuação = resultadoUser === resultadoComputer
+// // const menorPontuação = resultadoUser < resultadoComputer
+
+// // while (resultadoUser) {
+// //     if (maiorPontuação) {
+// //         resultadoUser++
+// //     }
+// // // }
